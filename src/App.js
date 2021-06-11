7844fd17a4d69bb6c7e08d433b980beb285853da
Janeé Mays <janee.elise@gmail.com>
2021-06-11 19:54:15 -0400

    custom greeting, input footer, created nav.js and projects.js files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
         <div>
             <Header />
+            <Footer />
         </div>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,26 @@
 import React from "react";
 
 
-export default Heading;
+function Header(){
+    const date = new Date();
+    const currentTime = date.getHours();
+
+    let greeting;
+
+    if (currentTime < 12) {
+        greeting = "Good Morning!";
+    } else if (currentTime < 18) {
+        greeting = "Good Afternoon!";
+    } else {
+        greeting = "Good Evening!";
+    }
+
+    
+    return (
+        <header>
+        <h1>{greeting} My name is Janeé Mays</h1>
+        </header>
+    );
+}
+
+export default Header;
